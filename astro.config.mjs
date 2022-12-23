a0fd05c25b8e89623fa13c9d0c5a727bfbbf5fcf
Yoav Tzfati <yoav.tzfati@gmail.com>
2022-12-23 21:45:38 +0200

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,5 +10,7 @@ import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://crazytieguy.github.io",
+  base: "/lior-and-ariel",
   integrations: [tailwind(), solidJs(), astroI18next()],
 });
